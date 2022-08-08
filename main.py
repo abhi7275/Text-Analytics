08dcb9c3fbd040743de25e1e8d6fee747edc113d
ABHISHEK KUMAR <42292137+abhi7275@users.noreply.github.com>
2022-08-09 03:18:48 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+import requests as req
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.textanalytics import TextAnalyticsClient
+import utils
+
+app = FastAPI()
+
+headers = {
+    "Ocp-Apim-Subscription-Key": 'da2dee8c93664abd9e21747a6b4aeb20',
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+}
+
+
+class Model(BaseModel):
+    text_to_analyze: list
+
+
+@app.post("/")
+def analyze_text(text: Model):
+    response = {"sentiment": [], "keyPhrases": []}
+    no_of_text = len(text.text_to_analyze)
+    for i in range(no_of_text):
+        document = {"documents": [{"id": i + 1, "language": "en", "text": text.text_to_analyze[i]}]}
+
+        sentiment = utils.call_text_analytics_api(headers, document, endpoint='sentiment')
+        keyPhrases = utils.call_text_analytics_api(headers, document, endpoint='keyPhrases')
+
+        response["sentiment"].append(sentiment["documents"][0])
+        response["keyPhrases"].append(keyPhrases["documents"][0])
+    return response
